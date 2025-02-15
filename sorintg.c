8ddbb91361754af4bf5b2f4b7bde7b2ec1ca699b
siddhijain120 <siddhijain061307@gmail.com>
2025-02-15 11:31:26 +0530

    Update bubblesorintg.c

--- sorintg.c
+++ sorintg.c
@@ -1,20 +1,21 @@
-//SORTING TECHNIQUES
 //Bubble Sort
 
 #include<stdio.h>
 
 void Bubble_Sort(int arr[], int n){
-int temp;
-for(int i = 0; i < n-1; i++){
-	for(int j = i+1; j < n-i; j++) {
-		if(arr[j-1] < arr[j]) {
-			temp = arr[j-1];
-			arr[j-1] = arr[j];
-			arr[j] = temp;
+	int temp;
+	for(int i = 0; i < n-1; i++){//runs from 0 to n-1
+		for(int j = 1; j < n-i; j++){//runs from 1 to n-i as after every pass the last element gets sorted
+			if(arr[j-1] > arr[j]){//swap if left element > right element
+				temp = arr[j-1];
+				arr[j-1] = arr[j];
+				arr[j] = temp;
+			}
 		}
 	}
 }
-	
+
+
 int main() {
 	int n;
 	scanf("%d",&n);
